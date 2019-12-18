12ef92f2cea1d755e1e1dc45d43e71ae18d0cb63
Gianluca Bine <gian_bine@hotmail.com>
2019-12-18 18:24:32 -0300

    Adicionado validacao nos campos do formulario

--- src/screens/StudentForm.vue
+++ src/screens/StudentForm.vue
@@ -14,12 +14,26 @@
 
         <nb-content>
             <nb-form>
-                <nb-item stackedLabel>
+                <nb-item stackedLabel
+                     :error="$v.name.$error && $v.name.$dirty"
+                     :success="!$v.name.$error && $v.name.$dirty"
+                >
                     <nb-label>Nome</nb-label>
-                    <nb-input v-model="student.name"/>
+                    <nb-input v-model="name" :on-blur="() => $v.name.$touch()" autoFocus autoCapitalize="words"/>
                 </nb-item>
 
-                <nb-item stackedLabel>
+                <nb-item stackedLabel
+                     :error="$v.grade.$error && $v.grade.$dirty"
+                     :success="!$v.grade.$error && $v.grade.$dirty"
+                >
+                    <nb-label>Serie de Ingresso</nb-label>
+                    <nb-input v-model="grade" :on-blur="() => $v.grade.$touch()" keyboardType="numeric"/>
+                </nb-item>
+
+                <nb-item stackedLabel
+                     :error="$v.birthdate.$error && $v.birthdate.$dirty"
+                     :success="!$v.birthdate.$error && $v.birthdate.$dirty"
+                >
                     <nb-label>Data de Nascimento</nb-label>
                     <nb-date-picker
                         :defaultDate="defaultDate"
@@ -28,71 +42,86 @@
                         :modalTransparent="false"
                         animationType="fade"
                         androidMode="default"
-                        :onDateChange="() => {}"
+                        :onDateChange="setBirthdate"
+                        :on-blur="() => $v.birthdate.$touch()"
                     />
                 </nb-item>
 
-                <nb-item stackedLabel>
-                    <nb-label>Serie de Ingresso</nb-label>
-                    <nb-picker
-                        mode="dropdown"
-                        :note="false"
-                        :style="{ width: 350 }"
-                        :selectedValue="student.grade"
-                        :onValueChange="value => { student.grade = value }"
-                    >
-                        <item :label="grade.label" :value="grade.value" v-for="grade in gradeOptions" :key="grade.label"></item>
-                    </nb-picker>
-                </nb-item>
-
-                <nb-item stackedLabel>
+                <nb-item stackedLabel
+                     :error="$v.cep.$error && $v.cep.$dirty"
+                     :success="!$v.cep.$error && $v.cep.$dirty"
+                >
                     <nb-label>CEP</nb-label>
-                    <nb-input v-model="student.cep"/>
+                    <nb-input v-model="cep" :on-complete="() => $v.cep.$touch()" keyboardType="numeric"/>
                 </nb-item>
 
-                <nb-item stackedLabel>
+                <nb-item stackedLabel
+                     :error="$v.street.$error && $v.street.$dirty"
+                     :success="!$v.street.$error && $v.street.$dirty"
+                >
                     <nb-label>Rua</nb-label>
-                    <nb-input v-model="student.street"/>
+                    <nb-input v-model="street" :on-blur="() => $v.street.$touch()"/>
                 </nb-item>
 
-                <nb-item stackedLabel>
+                <nb-item stackedLabel
+                     :error="$v.number.$error && $v.number.$dirty"
+                     :success="!$v.number.$error && $v.number.$dirty"
+                >
                     <nb-label>Numero</nb-label>
-                    <nb-input v-model="student.number"/>
+                    <nb-input v-model="number" :on-blur="() => $v.number.$touch()" keyboardType="numeric"/>
                 </nb-item>
 
                 <nb-item stackedLabel>
                     <nb-label>Complemento</nb-label>
-                    <nb-input v-model="student.complement"/>
+                    <nb-input v-model="complement" />
                 </nb-item>
 
-                <nb-item stackedLabel>
+                <nb-item stackedLabel
+                     :error="$v.district.$error && $v.district.$dirty"
+                     :success="!$v.district.$error && $v.district.$dirty"
+                >
                     <nb-label>Bairro</nb-label>
-                    <nb-input v-model="student.district"/>
+                    <nb-input v-model="district" :on-blur="() => $v.district.$touch()"/>
                 </nb-item>
 
-                <nb-item stackedLabel>
+                <nb-item stackedLabel
+                     :error="$v.city.$error && $v.city.$dirty"
+                     :success="!$v.city.$error && $v.city.$dirty"
+                >
                     <nb-label>Cidade</nb-label>
-                    <nb-input v-model="student.city"/>
+                    <nb-input v-model="city" :on-blur="() => $v.city.$touch()"/>
                 </nb-item>
 
-                <nb-item stackedLabel>
+                <nb-item stackedLabel
+                     :error="$v.state.$error && $v.state.$dirty"
+                     :success="!$v.state.$error && $v.state.$dirty"
+                >
                     <nb-label>Estado</nb-label>
-                    <nb-input v-model="student.state"/>
+                    <nb-input v-model="state" :on-blur="() => $v.state.$touch()"/>
                 </nb-item>
 
-                <nb-item stackedLabel>
+                <nb-item stackedLabel
+                     :error="$v.motherName.$error && $v.motherName.$dirty"
+                     :success="!$v.motherName.$error && $v.motherName.$dirty"
+                >
                     <nb-label>Nome da Mae</nb-label>
-                    <nb-input v-model="student.motherName"/>
+                    <nb-input v-model="motherName" :on-blur="() => $v.motherName.$touch()"/>
                 </nb-item>
 
-                <nb-item stackedLabel>
+                <nb-item stackedLabel
+                     :error="$v.motherCpf.$error && $v.motherCpf.$dirty"
+                     :success="!$v.motherCpf.$error && $v.motherCpf.$dirty"
+                >
                     <nb-label>CPF da Mae</nb-label>
-                    <nb-input v-model="student.motherCpf"/>
+                    <nb-input v-model="motherCpf" :on-blur="() => $v.motherCpf.$touch()" keyboardType="numeric"/>
                 </nb-item>
 
-                <nb-item stackedLabel last>
+                <nb-item stackedLabel
+                     :error="$v.preferredMonthlyPaymentDay.$error && $v.preferredMonthlyPaymentDay.$dirty"
+                     :success="!$v.preferredMonthlyPaymentDay.$error && $v.preferredMonthlyPaymentDay.$dirty"
+                >
                     <nb-label>Dia Pref. para Pagamento da Mensalidade</nb-label>
-                    <nb-input v-model="student.preferredMonthlyPaymentDay"/>
+                    <nb-input v-model="preferredMonthlyPaymentDay" :on-blur="() => $v.preferredMonthlyPaymentDay.$touch()" keyboardType="numeric"/>
                 </nb-item>
 
                 <nb-item class="action-buttons" full>
@@ -112,6 +141,14 @@
 import { mapActions, mapGetters } from 'vuex';
 import { Toast } from "native-base";
 import React from "react";
+import cep from "cep-promise";
+import { isCPF } from 'brazilian-values';
+import { required, maxLength, numeric } from "vuelidate/lib/validators"
+
+const gradeValidator = value => [1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number(value));
+const cepValidator = value => cep(value);
+const cpfValidator = value => isCPF(value);
+const preferredMonthlyPaymentDayValidator = value => [5, 10, 15, 20, 25].includes(Number(value));
 
 export default {
     props: {
@@ -119,7 +156,19 @@ export default {
     },
     data() {
         return {
-            student: {},
+            name: "",
+            birthdate: "",
+            grade: "",
+            cep: "",
+            street: "",
+            number: "",
+            complement: "",
+            district: "",
+            city: "",
+            state: "",
+            motherName: "",
+            motherCpf: "",
+            preferredMonthlyPaymentDay: "",
             defaultDate: new Date('2018-06-04'),
             minimumDate: new Date('2018-01-01'),
             maximumDate: new Date('2018-12-31'),
@@ -127,19 +176,44 @@ export default {
         }
     },
     created() {
-        this.student = { ...this.currentStudent };
+        this.$v.$reset();
+        this.loadFormValues();
     },
     methods: {
         ...mapActions(['addStudent', 'editStudent', 'resetCurrentStudent']),
+        loadFormValues() {
+            if (!this.currentStudent) {
+                return;
+            }
+
+            Object.assign(this, this.currentStudent);
+            this.$v.$touch();
+        },
         goToHomeScreen() {
             this.navigation.navigate("Home");
         },
+        setBirthdate(value) {
+            this.birthdate = value;
+            this.$v.birthdate.$touch();
+        },
         async saveStudent() {
+            this.$v.$touch();
+
+            if (this.$v.$invalid) {
+                Toast.show({
+                    text: "Por favor preencha todos os campos corretamente!",
+                    buttonText: "Ok",
+                    type: "danger",
+                });
+
+                return;
+            }
+
             try {
                 if (this.currentStudent.id || this.currentStudent.id === 0) {
-                    await this.editStudent(this.student);
+                    await this.editStudent(this.formData);
                 } else {
-                    await this.addStudent(this.student);
+                    await this.addStudent(this.formData);
                 }
 
                 Toast.show({
@@ -151,7 +225,7 @@ export default {
                 Toast.show({
                     text: "Falha ao salvar estudante!",
                     buttonText: "Ok",
-                    type: "error"
+                    type: "danger"
                 });
             } finally {
                 this.resetCurrentStudent();
@@ -172,8 +246,77 @@ export default {
             }
 
             return gradeOptions;
-        }
+        },
+        formData() {
+            return {
+                id: this.currentStudent ? this.currentStudent.id : null,
+                name: this.name,
+                birthdate: this.birthdate,
+                grade: this.grade,
+                cep: this.cep,
+                street: this.street,
+                number: this.number,
+                complement: this.complement,
+                district: this.district,
+                city: this.city,
+                state: this.state,
+                motherName: this.motherName,
+                motherCpf: this.motherCpf,
+                preferredMonthlyPaymentDay: this.preferredMonthlyPaymentDay,
+            }
+        },
     },
+    validations: {
+        name: {
+            required,
+            maxLength: maxLength(100),
+        },
+        birthdate: {
+            required
+        },
+        grade: {
+            required,
+            numeric,
+            gradeValidator,
+        },
+        cep: {
+            required,
+            cepValidator,
+        },
+        street: {
+            required,
+            maxLength: maxLength(120),
+        },
+        number: {
+            required,
+            numeric,
+        },
+        district: {
+            required,
+            maxLength: maxLength(100),
+        },
+        city: {
+            required,
+            maxLength: maxLength(100),
+        },
+        state: {
+            required,
+            maxLength: maxLength(2),
+        },
+        motherName: {
+            required,
+            maxLength: maxLength(100),
+        },
+        motherCpf: {
+            required,
+            cpfValidator
+        },
+        preferredMonthlyPaymentDay: {
+            required,
+            numeric,
+            preferredMonthlyPaymentDayValidator,
+        }
+    }
 }
 </script>
 
