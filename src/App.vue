395c50b30416c685425bce60f1a3e09ee1cb45c3
Gianluca Bine <gian_bine@hotmail.com>
2019-12-18 14:53:55 -0300

    Configuracao inicial basica

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <root>
+        <app-navigator></app-navigator>
+    </root>
+</template>
+
+<script>
+import {
+    createAppContainer,
+    createStackNavigator,
+} from "vue-native-router";
+
+import { Root } from "native-base";
+
+import StudentList from "./screens/StudentList";
+import StudentForm from "./screens/StudentForm";
+
+const StackNavigator = createStackNavigator(
+    {
+        Home: StudentList,
+        Form: StudentForm,
+    },
+    {
+        initialRouteName: "Home",
+        transitionConfig: () => ({
+            containerStyle: {
+                marginTop: -60,
+            }
+        }),
+    }
+);
+
+const AppNavigator = createAppContainer(StackNavigator);
+
+export default {
+    components: { Root, AppNavigator},
+}
+</script>
---
+++ src/Setup.vue
@@ -0,0 +1,27 @@
+<template>
+    <view class="container">
+        <app/>
+    </view>
+</template>
+
+<script>
+import Vue from "vue-native-core";
+import Vuelidate from 'vuelidate';
+import App from "./App.vue";
+import { VueNativeBase } from "native-base";
+import store from "./store";
+
+Vue.use(Vuelidate);
+Vue.use(VueNativeBase);
+Vue.prototype.$store = store;
+
+export default {
+    components: { App },
+};
+</script>
+
+<style>
+.container {
+    flex: 1;
+}
+</style>
