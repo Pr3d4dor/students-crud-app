ac92c4fc7b2127b2cc04e91c783184c86dea5e73
Gianluca Bine <gian_bine@hotmail.com>
2019-12-21 16:00:48 -0300

    Dados para pickers

---
+++ src/utils/grades.js
@@ -0,0 +1,39 @@
+export default
+[
+	{
+		"name": "1° Ano",
+		"value": "1",
+	},
+	{
+		"name": "2° Ano",
+		"value": "2",
+	},
+	{
+		"name": "3° Ano",
+		"value": "3",
+	},
+	{
+		"name": "4° Ano",
+		"value": "4",
+	},
+	{
+		"name": "5° Ano",
+		"value": "5",
+	},
+	{
+		"name": "6° Ano",
+		"value": "6",
+	},
+	{
+		"name": "7° Ano",
+		"value": "7",
+	},
+	{
+		"name": "8° Ano",
+		"value": "8",
+	},
+	{
+		"name": "9° Ano",
+		"value": "9",
+	},
+]
