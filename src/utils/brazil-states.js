af8975bc03ff8cd1361caa7a2874442526327fdd
Gianluca Bine <gian_bine@hotmail.com>
2019-12-21 15:54:42 -0300

    Dados para pickers

---
+++ src/utils/brazil-states.js
@@ -0,0 +1,83 @@
+export default
+[{
+	"value": "AC",
+	"name": "Acre"
+},{
+	"value": "AL",
+	"name": "Alagoas"
+},{
+	"value": "AM",
+	"name": "Amazonas"
+},{
+	"value": "AP",
+	"name": "Amapá"
+},{
+	"value": "BA",
+	"name": "Bahia"
+},{
+	"value": "CE",
+	"name": "Ceará"
+},{
+	"value": "DF",
+	"name": "Distrito Federal"
+},{
+	"value": "ES",
+	"name": "Espírito Santo"
+},{
+	"value": "GO",
+	"name": "Goiás"
+},{
+	"value": "MA",
+	"name": "Maranhão"
+},{
+	"value": "MG",
+	"name": "Minas Gerais"
+},{
+	"value": "MS",
+	"name": "Mato Grosso do Sul"
+},{
+	"value": "MT",
+	"name": "Mato Grosso"
+},{
+	"value": "PA",
+	"name": "Pará"
+},{
+	"value": "PB",
+	"name": "Paraíba"
+},{
+	"value": "PE",
+	"name": "Pernambuco"
+},{
+	"value": "PI",
+	"name": "Piauí"
+},{
+	"value": "PR",
+	"name": "Paraná"
+},{
+	"value": "RJ",
+	"name": "Rio de Janeiro"
+},{
+	"value": "RN",
+	"name": "Rio Grande do Norte"
+},{
+	"value": "RO",
+	"name": "Rondônia"
+},{
+	"value": "RR",
+	"name": "Roraima"
+},{
+	"value": "RS",
+	"name": "Rio Grande do Sul"
+},{
+	"value": "SC",
+	"name": "Santa Catarina"
+},{
+	"value": "SE",
+	"name": "Sergipe"
+},{
+	"value": "SP",
+	"name": "São Paulo"
+},{
+	"value": "TO",
+	"name": "Tocantins"
+}]
---
+++ src/utils/preferred-days-for-monthly-payment.js
@@ -0,0 +1,23 @@
+export default
+[
+    {
+        "name": "Dia 5",
+        "value": "5",
+    },
+    {
+        "name": "Dia 10",
+        "value": "10",
+    },
+    {
+        "name": "Dia 15",
+        "value": "15",
+    },
+    {
+        "name": "Dia 20",
+        "value": "20",
+    },
+    {
+        "name": "Dia 25",
+        "value": "25",
+    },
+]
